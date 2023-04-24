730022a1e45edb30bb24cfac90e363f22e564326
Basheer Rajeh <muhammadbasheerrajeh@hotmail.com>
2023-04-24 10:49:43 +0300

    Refactore: remove duplicated styles

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,4 +1,4 @@
-import { Card, CardBody, HStack, Heading, Image } from "@chakra-ui/react";
+import {  Card, CardBody, HStack, Heading, Image } from "@chakra-ui/react";
 import { Game } from "../hooks/useGames";
 import PlatformIconList from "./PlatformIconList";
 import CriticScore from "./CriticScore";
@@ -6,7 +6,7 @@ import getCroppedImageUrl from "../services/image-url";
 
 const GameCard = ({ game }: Props) => {
     return (
-        <Card borderRadius={10} width='300px' overflow="hidden">
+        <Card>
             <Image src={getCroppedImageUrl(game.background_image)} />
             <CardBody>
                 <Heading fontSize="2xl">{game.name}</Heading>
