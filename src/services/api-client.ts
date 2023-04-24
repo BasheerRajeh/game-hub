48c96192581b526cdce3cc881e9f2e97c12a55b6
Basheer Rajeh <muhammadbasheerrajeh@hotmail.com>
2023-04-24 17:24:06 +0300

    Change app icon and title

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -3,6 +3,6 @@ import axios from "axios";
 export default axios.create({
     baseURL: "https://api.rawg.io/api",
     params: {
-        key: "395c7d327fd144cd828a168b6c866009",
+        key: `${import.meta.env.VITE_APP_SECRET_VAR}`,
     },
 });
