a6aa120a664d313858388c75b10107235e394bef
Basheer Rajeh <muhammadbasheerrajeh@hotmail.com>
2023-04-24 15:17:13 +0300

    Add icons for color mode switch

--- src/components/ColorModeSwitch.tsx
+++ src/components/ColorModeSwitch.tsx
@@ -1,11 +1,12 @@
-import { HStack, Switch, Text, useColorMode } from "@chakra-ui/react";
+import { HStack, Icon, Switch, Text, useColorMode } from "@chakra-ui/react";
+import { FaMoon, FaSun } from "react-icons/fa";
 
 const ColorModeSwitch = () => {
   const { colorMode, toggleColorMode } = useColorMode();
 
   return (
     <HStack>
-      <Text>{colorMode} mode</Text>
+      <Icon as={colorMode === "dark" ? FaMoon : FaSun} boxSize='24px' />
       <Switch
         colorScheme="green"
         isChecked={colorMode === "dark"}
