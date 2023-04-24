242f3e0166a8e10ab9dc5ed00d99994f104a948d
Basheer Rajeh <muhammadbasheerrajeh@hotmail.com>
2023-04-24 15:48:31 +0300

    Clean up the genres

--- src/components/GameHeading.tsx
+++ src/components/GameHeading.tsx
@@ -4,7 +4,11 @@ import { GameQuery } from "../App";
 const GameHeading = ({ gameQuery }: Props) => {
     const heading = `${gameQuery.platform?.name || ""} 
     ${gameQuery.genre?.name || ""} Games`;
-    return <Heading as="h1" marginY={5} fontSize='5xl'>{heading}</Heading>;
+    return (
+        <Heading as="h1" marginY={5} fontSize="5xl">
+            {heading}
+        </Heading>
+    );
 };
 
 export default GameHeading;
