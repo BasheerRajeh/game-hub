d5c636b7f64ec22aa9490e060a0d7e0577b5e3e6
Basheer Rajeh <muhammadbasheerrajeh@hotmail.com>
2023-04-24 10:07:01 +0300

    Displaying platform icons

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -24,10 +24,17 @@ const useGames = () => {
 
 export default useGames;
 
+export interface Platform {
+    id: number;
+    name: string;
+    slug: string;
+}
+
 export interface Game {
     id: number;
     name: string;
     background_image: string;
+    parent_platforms: { platform: Platform }[];
 }
 
 interface FetchGamesResponse {
